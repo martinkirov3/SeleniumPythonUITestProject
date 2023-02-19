fefbd5c13fd9e9f07573cb26dae5c6a3894e99d2
wildM1s <32509012+martinkirov3@users.noreply.github.com>
2023-02-19 20:11:36 +0200

    Delete Ryan directory

--- Ryan/ryanair/src/test/java/feature/ApiTestRyan.java
+++
@@ -1,8 +0,0 @@
-package feature;
-
-/* import com.intuit.karate.junit5.Karate; */
-import com.intuit.karate.junit4.Karate;
-import org.junit.runner.RunWith;
-@RunWith(Karate.class)
-public class ApiTestRyan {
-}
--- Ryan/ryanair/target/test-classes/karate-config.js
+++
@@ -1,9 +0,0 @@
- function() {   
-  var config = {
-    apiURL: 'http://127.0.0.1:8900/'
-  };
-  
-  karate.configure('connectTimeout', 150000);
-  karate.configure('readTimeout', 150000);
-  return config;
-}
